5a13620b8bce5f51f0f238fa16136d1711231d75
Shakeel Mohamed <smohamed@splunk.com>
2015-11-29 00:42:29 -0800

    Very minimalist working prototype!

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+
+var mediaPath = path.join(__dirname, "..", "public", "media");
+var videoFormats = ["mp4", "webm", "ogg"];
+
+function getVideoFiles() {
+  var files = fs.readdirSync(mediaPath);
+  var videoFiles = [];
+  for (var f in files) {
+    for (var v in videoFormats) {
+        if (files[f].indexOf(videoFormats[v]) === (files[f].length - 3)) {
+            videoFiles.push({
+                format: videoFormats[v],
+                path: path.basename(mediaPath) + "/" + files[f]
+            });
+            break;
+        }
+    }    
+  }
+  return videoFiles;
+}
 
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  var videoFiles = getVideoFiles();
+
+  res.render('index', { title: 'Raspberry Media Express', videoFiles: videoFiles });
 });
 
 module.exports = router;
